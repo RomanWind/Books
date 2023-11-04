47e19e7f35d88a2c8f3bd63d239886ef39381ab8
Roman Wind <119047529+RomanWind@users.noreply.github.com>
2023-11-04 12:44:27 +0100

    Add project files.

---
+++ NovemberBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NovemberBooks.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
